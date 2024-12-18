c6078f0a06ae13504ff80b3fc9e0d46df12e5a1c
Aryan_Vekariya <aaryanvekariya0143@gmail.com>
2024-12-18 12:18:49 +0530

    fix errors

---
+++ main.js
@@ -0,0 +1,3 @@
+let a  = 6.023
+console.log(a==6.023)
+
--- task1/main.js
+++ task1/main.js
@@ -1,13 +1,11 @@
 function show() {
-    
     let name = document.getElementById("myName").value;
     let age = document.getElementById("myAge").value;
     let div = document.getElementById("myDiv").value;
     let email = document.getElementById("myMail").value;
-    let rating = document.getElementById("rate").value;
+    let rating = document.getElementById("rateSelect").value;
     let cmt = document.getElementById("myComment").value;
 
-    
     let output = `
     Feedback Details:
     Name: ${name}
@@ -17,7 +15,7 @@ function show() {
     Rating: ${rating}
     Comment: ${cmt}
     `;
+    console.log(output);
 
-
-    document.getElementById("myH1").innerText = output;
+    document.getElementById("myH1").innerText = `Thank you ${name} for submitting feedback`;
 }
